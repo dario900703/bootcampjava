98512f3ee696768822a61fff2fc9dd27a14b336c
dario900703 <rubend1990@gmail.com>
2022-06-29 11:04:54 -0500

    Se sube la practica de html, css y js

---
+++ cronometro/script/script.js
@@ -0,0 +1,75 @@
+let centesimas = 0;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let sonido = new Audio('audio/pito.wav');
+
+
+
+
+
+function inicio () {
+	control = setInterval(cronometro,10);
+	document.getElementById("inicio").disabled = true;
+	document.getElementById("pausar").disabled = false;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reiniciar").disabled = false;
+
+    
+}
+function parar () {
+	clearInterval(control);
+	document.getElementById("pausar").disabled = true;
+	document.getElementById("continuar").disabled = false;
+    
+}
+function reinicio () {
+	clearInterval(control);
+	centesimas = 0;
+	segundos = 0;
+	minutos = 0;
+	horas = 0;
+	Centesimas.innerHTML = ":00";
+	Segundos.innerHTML = ":00";
+	Minutos.innerHTML = ":00";
+	Horas.innerHTML = "00";
+	document.getElementById("inicio").disabled = false;
+	document.getElementById("parar").disabled = true;
+	document.getElementById("continuar").disabled = true;
+	document.getElementById("reiniciar").disabled = true;
+}
+function cronometro () {
+	if (centesimas < 99) {
+		centesimas++;
+		if (centesimas < 10) { centesimas = "0"+centesimas }
+		Centesimas.innerHTML = ":"+centesimas;
+	}
+	if (centesimas == 99) {
+		centesimas = -1;
+	}
+	if (centesimas == 0) {
+		segundos ++;
+        sonido.play();
+		if (segundos < 10) { segundos = "0"+segundos }
+		Segundos.innerHTML = ":"+segundos;
+	}
+	if (segundos == 59) {
+		segundos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos++;
+		if (minutos < 10) { minutos = "0"+minutos }
+		Minutos.innerHTML = ":"+minutos;
+	}
+	if (minutos == 59) {
+		minutos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+		horas ++;
+		if (horas < 10) { horas = "0"+horas }
+		Horas.innerHTML = horas;
+	}
+}
+
+
+
